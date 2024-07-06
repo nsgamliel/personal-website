c8f82212456760508a50003db2f49fbf6ca811be
nsgamliel <nsgamliel@gmail.com>
2024-07-05 23:39:34 -0400

    ui updates, slight refactor for simplicity

---
+++ public/content.js
@@ -0,0 +1,49 @@
+const content = {
+	'edu': {
+		'full': 'education',
+		'chunk': `
+			<p>Northwestern University | Evanston, IL</p>
+			<p>- Bachelor of Science in <span class="intro-edu">Computer Science</span>, McCormick School of Engineering<br>- Certificate Program for Undergraduates in <span class="intro-edu">Managerial Analytics</span></p>
+		`,
+	},
+	'exp': {
+		'full': 'experience',
+		'chunk': `
+			<div style="margin-bottom: 1em;">
+				<a class="intro-exp" href="pdfs/GamlielNatanResume_2024.5.pdf" target="_blank">View my full resume</a>
+			</div>
+			<div class="exp-item">
+				<p><strong>Project Coordinator</strong> @ Massachusetts Bay Transit Authority<br>April 2024 - Present</p>
+				<p class="exp-tags"><span class="intro-exp">!</span>Project_management <span class="intro-exp">!</span>Asana</p>
+			</div>
+			<div class="exp-item">
+				<p><strong>Research Assistant</strong> @ District 65 Research Group<br>March 2021 - June 2023</p>
+				<p class="exp-tags"><span class="intro-exp">!</span>Python <span class="intro-exp">!</span>Flask <span class="intro-exp">!</span>HTML <span class="intro-exp">!</span>CSS <span class="intro-exp">!</span>JavaScript <span class="intro-exp">!</span>GitHub <span class="intro-exp">!</span>Mapbox_GL <span class="intro-exp">!</span>Data_visualization</p>
+			</div>
+			<div class="exp-item">
+				<p><strong>Chief Technical Officer</strong> @ Student Holdings<br>November 2021 - February 2023</p>
+				<p class="exp-tags"><span class="intro-exp">!</span>React <span class="intro-exp">!</span>Firebase <span class="intro-exp">!</span>GitHub <span class="intro-exp">!</span>Project_management <span class="intro-exp">!</span>Squarespace <span class="intro-exp">!</span>Zapier <span class="intro-exp">!</span>Google_Admin <span class="intro-exp">!</span>Student_startup</p>
+			</div>
+			<div class="exp-item">
+				<p><strong>Intern</strong> @ Publicis Sapient<br>June 2022 - August 2022</p>
+				<p class="exp-tags"><span class="intro-exp">!</span>Spring_Boot <span class="intro-exp">!</span>Java <span class="intro-exp">!</span>Microsoft_Asure <span class="intro-exp">!</span>React <span class="intro-exp">!</span>Jira <span class="intro-exp">!</span>Agile_methodology <span class="intro-exp">!</span>GitHub</p>
+			</div>
+			<div class="exp-item">
+				<p><strong>Intern</strong> @ Robotic Perception<br>June 2021 - August 2021</p>
+				<p class="exp-tags"><span class="intro-exp">!</span>Computer_Vision <span class="intro-exp">!</span>Machine_Learning <span class="intro-exp">!</span>Jupyter_Notebook <span class="intro-exp">!</span>Startup</p>
+			</div>
+			<div class="exp-item">
+				<p><strong>Software Engineer</strong> @ Scope<br>November 2019 - September 2020</p>
+				<p class="exp-tags"><span class="intro-exp">!</span>Google_Maps_Platform <span class="intro-exp">!</span>Firebase <span class="intro-exp">!</span>HTML <span class="intro-exp">!</span>JavaScript <span class="intro-exp">!</span>CSS <span class="intro-exp">!</span>Project_management <span class="intro-exp">!</span>Student_startup</p>
+			</div>
+		`,
+	},
+	'cont': {
+		'full': 'contact',
+		'chunk': `
+			<p>Feel free to reach out over email (<span class="intro-cont">nsgamliel [at] gmail [dot] com</span>) or <a class="intro-cont" href="https://www.linkedin.com/in/natan-gamliel/" target="_blank">connect with me via linkedin</a>.</p>
+		`,
+	}
+};
+
+export { content };
--- public/script.js
+++ public/script.js
@@ -2,295 +2,93 @@
 // const heroDate = document.querySelector('.intro').querySelector('.timeline-date');
 // heroDate.innerHTML = today.toDateString();
 
+import { content } from "./content.js";
+
 const blink = async () => {
 	await new Promise(resolve => setInterval(resolve, 400));
 
-	const intro = document.getElementById('intro-phrase');
-
 	while (true) {//for (let i=0; i<4; i++) {
-		const introText = intro.innerHTML;
-		if (introText.charAt(introText.length-1) == '_') {
-			intro.innerHTML = introText.slice(0,-1);
+		const prompt = document.querySelector('.prompt.blink');
+		if (!prompt) break;
+		const promptText = prompt.innerHTML;
+		if (promptText.charAt(promptText.length-1) == '_') {
+			prompt.innerHTML = promptText.slice(0,-1);
 		} else {
-			intro.innerHTML += '_';
+			prompt.innerHTML += '_';
 		}
 		await new Promise(resolve => setInterval(resolve, 500));
 	}
 };
 
 const typeIntro = async () => {
+	const intro = document.getElementById('intro-phrase');
 	const text = `Hi, I'm Natan`;
 	
 	for (let i=1; i<=text.length; i++) {
-		const intro = document.getElementById('intro-phrase');
 		intro.innerHTML = `>${text.substring(0,i)}_`;
-		if (i == 3) await new Promise(resolve => setInterval(resolve, 300));
-		await new Promise(resolve => setInterval(resolve, 100));
-	}
-
-	blink();
-	// await new Promise(resolve => setInterval(resolve, 200));
-
-	const elem = document.querySelector('.o-chunk');
-	elem.classList.add('fade-in');
-};
-
-const typeMessageById = async (id, text) => {
-	const elems = document.querySelectorAll(id);
-	const elem = elems[elems.length-1];
-
-	for (let i=0; i<=text.length; i++) {
-		elem.innerHTML = `> ${text.substring(0,i)}_`;
+		if (i == 3) await new Promise(resolve => setInterval(resolve, 150));
 		await new Promise(resolve => setInterval(resolve, 50));
 	}
-	elem.innerHTML = elem.innerHTML.slice(0,-1);
-};
 
-const handleOptMouseEnter = (event) => {
-	const text = event.target.innerHTML;
-	event.target.innerHTML = `[*]${text.substring(3,text.length)}`;
-};
+	intro.innerHTML = intro.innerHTML.slice(0,-1);
 
-const handleOptMouseLeave = (event) => {
-	const elemClasses = event.target.classList;
-	if (elemClasses.contains('edu')) {
-		event.target.innerHTML = '[ ]&nbsp; education';
-	} else if (elemClasses.contains('exp')) {
-		event.target.innerHTML = '[ ]&nbsp; experience';
-	} else if (elemClasses.contains('cont')) {
-		event.target.innerHTML = '[ ]&nbsp; contact';
-	}
-};
+	// await new Promise(resolve => setInterval(resolve, 200));
 
-const addIntroOptListeners = () => {
-	const currentOptions = document.querySelector('.more-options.current');
-	const introOptions = currentOptions.querySelectorAll('.intro-option');
-
-	introOptions.forEach((elem) => {
-		elem.addEventListener(
-			'mouseenter',
-			handleOptMouseEnter
-		);
-		elem.addEventListener(
-			'mouseleave',
-			handleOptMouseLeave
-		);
+	const elems = document.querySelectorAll('.o-chunk');
+	elems.forEach(elem => {
+		elem.classList.add('fade-in');
 	});
+	
+	blink(true);
 };
 
-const addEducation = async () => {
-	const feed = document.querySelector('.feed');
-	const prompt = document.createElement('p');
-	const chunk = document.createElement('div');
+const typePrompt = async (text) => {
+	const prompt = document.querySelector('.prompt');
 
-	// mute previous options
-	const prev = document.querySelector('.more-options.current');
-	prev.classList.remove('current');
-	const clicked = prev.querySelector('.intro-option.edu');
-	clicked.removeEventListener('mouseenter', handleOptMouseEnter);
-	clicked.removeEventListener('mouseleave', handleOptMouseLeave);
-	const text = clicked.innerHTML;
-	clicked.innerHTML = `[*]${text.substring(3,text.length)}`;
-	clicked.classList.remove('intro-option');
-	clicked.classList.add('option-clicked');
-	clicked.removeAttribute('onclick');
-	const sibs = prev.querySelectorAll('.intro-option');
-	for (const sib of sibs) {
-		sib.removeEventListener('mouseenter', handleOptMouseEnter);
-		sib.removeEventListener('mouseleave', handleOptMouseLeave);
-		sib.classList.remove('intro-option');
-		sib.classList.add('clicked-sibling');
-		sib.removeAttribute('onclick');
+	for (let i=0; i<=text.length; i++) {
+		prompt.innerHTML = `> ${text.substring(0,i)}_`;
+		await new Promise(resolve => setInterval(resolve, 50));
 	}
+	if (prompt.innerHTML.slice(-1) === '_') prompt.innerHTML = prompt.innerHTML.slice(0,-1);
+};
 
-	prompt.classList.add('edu');
-	chunk.classList.add('o-chunk');
-	chunk.classList.add('fade-in');
+const addContent = async (key) => {
+	const options = document.querySelector('.more-options');
+	const output = document.querySelector('.output');
+	const prompt = output.querySelector('.prompt');
+	const chunk = output.querySelector('.chunk');
 
-	prompt.innerHTML = '>';
-	feed.appendChild(prompt);
-	const offset = prompt.getBoundingClientRect().top - 68 + window.scrollY;
-	await typeMessageById('.edu', ' education');
-
-	chunk.innerHTML = `
-		<p>Northwestern University | Evanston, IL</p>
-		<p>- Bachelor of Science in <span class="intro-edu">Computer Science</span>, McCormick School of Engineering<br>- Certificate Program for Undergraduates in <span class="intro-edu">Managerial Analytics</span></p>
-		
-		<div class="more-options current">
-			<div class="intro-option edu" onclick="addEducation()">[ ]&nbsp; education</div>
-			<div class="intro-option exp" onclick="addExperience()">[ ]&nbsp; experience</div>
-			<div class="intro-option cont" onclick="addContact()">[ ]&nbsp; get_in_touch</div>
-		</div>
-	`;
-	feed.appendChild(chunk);
-	scrollTo({
-		top: offset,
-		left: 0,
-		behavior: 'smooth'
+	const selected = options.querySelector(`.intro-option.${key}`);
+	selected.parentNode.childNodes.forEach(node => {
+		if (node.classList && node.classList.contains('option-clicked')) node.classList.remove('option-clicked');
+		const text = node.innerHTML;
+		if (text) node.innerHTML = `[ ]${text.substring(3,text.length)}`;
 	});
-	addIntroOptListeners();
-};
-
-const addExperience = async () => {
-	const exps = [
-		{
-			'role': 'Research Assistant',
-			'firm': 'District 65 Research Group',
-			'date': 'March 2021 - June 2023',
-			'tags': [
-				'Python', 'Flask', 'HTML', 'CSS', 'JavaScript', 'GitHub', 'Mapbox_GL', 'Data_visualization'
-			]
-		},
-		{
-			'role': 'Chief Technical Officer',
-			'firm': 'Student Holdings',
-			'date': 'November 2021 - February 2023',
-			'tags': [
-				'React', 'Firebase', 'GitHub', 'Project_management', 'Squarespace', 'Zapier', 'Google_Admin', 'Student_startup'
-			]
-		},
-		{
-			'role': 'Intern',
-			'firm': 'Publicis Sapient',
-			'date': 'June 2022 - August 2022',
-			'tags': [
-				'Spring_Boot', 'Java', 'Microsoft_Asure', 'React', 'Jira', 'Agile_methodology', 'GitHub'
-			]
-		},
-		{
-			'role': 'Intern',
-			'firm': 'Robotic Perception',
-			'date': 'June 2021 - August 2021',
-			'tags': [
-				'Computer_Vision', 'Machine_Learning', 'Jupyter_Notebook', 'Startup'
-			]
-		},
-		{
-			'role': 'Software Engineer',
-			'firm': 'Scope',
-			'date': 'November 2019 - September 2020',
-			'tags': [
-				'Google_Maps_Platform', 'Firebase', 'HTML', 'JavaScript', 'CSS', 'Project_management', 'Student_startup'
-			]
-		}
-	];
-
-	const feed = document.querySelector('.feed');
-	const prompt = document.createElement('p');
-	const chunk = document.createElement('div');
-
-	// mute previous options
-	const prev = document.querySelector('.more-options.current');
-	prev.classList.remove('current');
-	const clicked = prev.querySelector('.intro-option.exp');
-	clicked.removeEventListener('mouseenter', handleOptMouseEnter);
-	clicked.removeEventListener('mouseleave', handleOptMouseLeave);
-	const text = clicked.innerHTML;
-	clicked.innerHTML = `[*]${text.substring(3,text.length)}`;
-	clicked.classList.remove('intro-option');
-	clicked.classList.add('option-clicked');
-	clicked.removeAttribute('onclick');
-	const sibs = prev.querySelectorAll('.intro-option');
-	for (const sib of sibs) {
-		sib.removeEventListener('mouseenter', handleOptMouseEnter);
-		sib.removeEventListener('mouseleave', handleOptMouseLeave);
-		sib.classList.remove('intro-option');
-		sib.classList.add('clicked-sibling');
-		sib.removeAttribute('onclick');
-	}
-
-	prompt.classList.add('exp');
-	chunk.classList.add('o-chunk');
-	chunk.classList.add('fade-in');
-
-	prompt.innerHTML = '>';
-	feed.appendChild(prompt);
-	const offset = prompt.getBoundingClientRect().top - 68 + window.scrollY;
-	await typeMessageById('.exp', ' experience');
+	selected.classList.add('option-clicked');
+	const text = selected.innerHTML;
+	selected.innerHTML = `[*]${text.substring(3,text.length)}`;
 
 	chunk.innerHTML = '';
-	for (const exp of exps) {
-		chunk.innerHTML += `
-			<div class="exp-item">
-				<p><strong>${exp.role}</strong> @ ${exp.firm}<br>${exp.date}</p>
-				<p class="exp-tags">${exp.tags.map((tag) => `<span class="intro-exp">!</span>${tag}`).join(' ')}</p>
-			</div>
-		`;
-	}
-
-	chunk.innerHTML += `
-		<div class="more-options current">
-			<div class="intro-option edu" onclick="addEducation()">[ ]&nbsp; education</div>
-			<div class="intro-option exp" onclick="addExperience()">[ ]&nbsp; experience</div>
-			<div class="intro-option cont" onclick="addContact()">[ ]&nbsp; get_in_touch</div>
-		</div>
-	`;
-	feed.appendChild(chunk);
-	scrollTo({
-		top: offset,
-		left: 0,
-		behavior: 'smooth'
-	});
-	addIntroOptListeners();
-};
-
-const addContact = async () => {
-	const feed = document.querySelector('.feed');
-	const prompt = document.createElement('p');
-	const chunk = document.createElement('div');
-
-	// mute previous options
-	const prev = document.querySelector('.more-options.current');
-	prev.classList.remove('current');
-	const clicked = prev.querySelector('.intro-option.cont');
-	clicked.removeEventListener('mouseenter', handleOptMouseEnter);
-	clicked.removeEventListener('mouseleave', handleOptMouseLeave);
-	const text = clicked.innerHTML;
-	clicked.innerHTML = `[*]${text.substring(3,text.length)}`;
-	clicked.classList.remove('intro-option');
-	clicked.classList.add('option-clicked');
-	clicked.removeAttribute('onclick');
-	const sibs = prev.querySelectorAll('.intro-option');
-	for (const sib of sibs) {
-		sib.removeEventListener('mouseenter', handleOptMouseEnter);
-		sib.removeEventListener('mouseleave', handleOptMouseLeave);
-		sib.classList.remove('intro-option');
-		sib.classList.add('clicked-sibling');
-		sib.removeAttribute('onclick');
-	}
-
-	prompt.classList.add('cont');
-	chunk.classList.add('o-chunk');
-	chunk.classList.add('fade-in');
-
 	prompt.innerHTML = '>';
-	feed.appendChild(prompt);
-	const offset = prompt.getBoundingClientRect().top - 68 + window.scrollY;
-	await typeMessageById('.cont', ' get_in_touch');
+	await typePrompt(` ${content[key]['full']}`);
+	prompt.classList.remove('blink');
 
-
-	chunk.innerHTML = `
-		<p>Feel free to reach out over email (<span class="intro-cont">gamlielnatan at gmail dot com</span>) or <a class="intro-cont" href="https://www.linkedin.com/in/natan-gamliel/" target="_blank">connect with me via linkedin</a>.</p>
-		<div class="more-options current">
-			<div class="intro-option edu" onclick="addEducation()">[ ]&nbsp; education</div>
-			<div class="intro-option exp" onclick="addExperience()">[ ]&nbsp; experience</div>
-			<div class="intro-option cont" onclick="addContact()">[ ]&nbsp; get_in_touch</div>
-		</div>
-	`;
-	feed.appendChild(chunk);
-	scrollTo({
-		top: offset,
-		left: 0,
-		behavior: 'smooth'
-	});
-	addIntroOptListeners();
+	chunk.innerHTML = content[key]['chunk'];
 };
 
 
 window.onload = (event) => {
 	console.log('page fully loaded.');
 	typeIntro();
-	addIntroOptListeners();
+	document.querySelector('.intro-option.edu').addEventListener('click', () => {
+		addContent('edu');
+	});
+	document.querySelector('.intro-option.exp').addEventListener('click', () => {
+		addContent('exp');
+	});
+	document.querySelector('.intro-option.cont').addEventListener('click', () => {
+		addContent('cont');
+	});
 };
 
 window.onscroll = (event) => {
